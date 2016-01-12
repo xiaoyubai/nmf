3d91596e49d840cdf19f3f61388acdcea7f05d05
xiaoyubai <hawkingbai@gmail.com>
2016-01-11 17:18:56 -0800

    Update nmf.py

--- nmf.py
+++ nmf.py
@@ -7,8 +7,16 @@ from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import NMF as skNMF
 
 class NMF(object):
-
+    """
+    Create a NMF class to that is initialized with a document matrix (bag of words or tf-idf) V.
+    It also take parameters k (# of latent topics) and the maximum # of iterations to perform
+    """
     def __init__(self, V, latent_topics, max_iter=20, error=0.01):
+        """
+        Initialize our weights (W) and features (H) matrices
+        Initialize the weights matrix (W) with (positive) random values to be a n x k matrix, where n is the number of documents and k is the number of latent topics
+        Initialize the feature matrix (H) to be k x m where m is the number of words in our vocabulary (i.e. length of bag).
+        """
         self.V = V
         self.k = latent_topics
         self.max_iter = max_iter
@@ -17,15 +25,24 @@ class NMF(object):
         self.error = error
 
     def update_H(self):
+        """
+        holding W fixed and minimizing the sum of squared errors predicting the document matrix
+        """
         self.H = lstsq(self.W, self.V)[0]
         self.H[self.H<0] = 0
 
     def update_W(self):
+        """
+        Use the same lstsq solver to update W while holding H fixed
+        """
         W_T = lstsq(self.H.T, self.V.T)[0]
         self.W = W_T.T
         self.W[self.W<0] = 0
 
     def fit(self):
+        """
+        Repeat update_H and update_W for a fixed number of iterations, or until convergence (i.e. change in cost(V, W*H) close to 0)
+        """
         for i in xrange(self.max_iter):
             if self.cost() > self.error:
                 print "itr %d | current cost: %f" % (i, self.cost())
@@ -35,6 +52,9 @@ class NMF(object):
                 print "min cost met"
 
     def cost(self):
+        """
+        cost function is mean squared error
+        """
         return mean_squared_error(self.V, np.dot(self.W, self.H))
 
     def key_feat_idx(self):
@@ -42,6 +62,9 @@ class NMF(object):
 
 
 def load_data(file, X_col, y_col):
+    """
+    Load, tokenize and vectorize data
+    """
     df = pd.read_pickle(file)
     X = df[X_col]
     y = df[y_col]
@@ -50,6 +73,9 @@ def load_data(file, X_col, y_col):
     return df, X, y, tok, vect
 
 def main():
+    """
+    Test function
+    """
     print "---INITIALIZE VARIABLES AND LOAD DATA---"
     df, X, y, tok, vect = load_data('data/articles.pkl', 'content', 'section_name')
     feature_name = np.array(vect.get_feature_names())
